306cf7826bed32c1f5ae5d547c6d88aae9b0938b
Ambesawi <ambesawi77@gmail.com>
2023-07-24 12:29:07 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
-Your web application must be listening on 0.0.0.0, port 5000
-"""
-
+"""class Flask"""
 from flask import Flask
+"""class Flask"""
 
-app = Flask("__name__")
+
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
+def hello_hbnb():
+    """displays text
+    Returns:
+        text
+    """
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+    app.run(host='0.0.0.0', port=5000)
